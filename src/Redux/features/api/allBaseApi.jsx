86f559be1e4e6f8a612d53eb7aa3438e3870a078
junayet <muhammadjunayetmaruf@gmail.com>
2024-10-01 10:13:31 +0300

    redux and rtk query has setup

---
+++ src/Redux/features/api/allBaseApi.jsx
@@ -0,0 +1,81 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+import { backendUrl } from "../../../backendUrl";
+
+const baseQuery = async (args) => {
+    try {
+        const response = await axios({
+            method: args.method,
+            url: `${backendUrl}${args.url}`,
+            data: args.body || undefined,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || 'Network response was not ok');
+    }
+};
+
+const allBaseApi = createApi({
+    reducerPath: 'allBaseApi',
+    baseQuery,
+    endpoints: (builder) => ({
+        getAllProperties: builder.query({
+            query: () => ({
+                url: `/properties`,
+                method: 'GET',
+            }),
+            providesTags: ['Properties'],
+        }),
+        queryProperties: builder.mutation({
+            query: (body) => ({
+                url: '/queryProperties',
+                method: 'POST',
+                body: body,
+            }),
+            invalidatesTags: ['Properties'],
+        }),
+        getSingleProperty: builder.query({
+            query: (id) => ({
+                url: `/properties/${id}`,
+                method: 'GET',
+            }),
+        }),
+        addProperty: builder.mutation({
+            query: (property) => ({
+                url: '/properties',
+                method: 'POST',
+                body: property,
+            }),
+            invalidatesTags: ['Properties'],
+        }),
+        updateProperty: builder.mutation({
+            query: ({ data, id }) => ({
+                url: `/properties/${id}`,
+                method: 'PUT',
+                body: data,
+            }),
+            invalidatesTags: ['Properties'],
+        }),
+        deleteProperty: builder.mutation({
+            query: (id) => ({
+                url: `/properties/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Properties'],
+        }),
+    })
+});
+
+export const {
+    useGetAllPropertiesQuery,
+    useQueryPropertiesMutation,
+    useGetSinglePropertyQuery,
+    useAddPropertyMutation,
+    useUpdatePropertyMutation,
+    useDeletePropertyMutation,
+} = allBaseApi;
+
+export default allBaseApi;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,23 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
+import 'swiper/css/free-mode';
+import 'swiper/css/thumbs';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { router } from './Routes/Routes.jsx';
+import store from './Redux/store.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <Toaster />
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>,
 )
